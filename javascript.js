1db5866ba29c850afabd7a6489810f845695f036
Shahabas Abdul Hameed <96893016+S488U@users.noreply.github.com>
2022-08-30 20:02:36 +0530

    Codes
    added my first code for my very very simple project .

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function btnPress(){
+    document.getElementById("search-text").value=innerHTML("displaying-text").text
+}
+function darkFunction(){
+    document.getElementById("dark").style.display="background-color: white;"
+}
